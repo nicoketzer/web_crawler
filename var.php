0445155e10252bb5f50546a5e52b316b4ae9d10a
Nico Ketzer <nicoketzer123@gmail.com>
2020-06-25 21:52:19 +0200

    Update var.php
    Alle Variablen hinzufügen in var.php

--- var.php
+++ var.php
@@ -3,5 +3,13 @@
     $db_pw = "[DATENBANK_PASSWORT]";
     $db_tbl = "[DATENBANK_TABELLE]";
     $db_conn = "[DATENBANK_IP]";
+    $db_port = "[DATENBANK_PORT]";
     $name = "[DIR_FOR_RUN_FILE]";
+    $main_crawler = "web";
+    $user_agent = "web_crawler_by_nicoketzer/github";
+    $timeout = 20;
+    $transmit_data = true;
+    $transmit_node_data = false;
+    $transmit_error = true;
+    $update_auto = false;
 ?>
