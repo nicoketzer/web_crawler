d02430dd6421129678d357733e2fba21f8ac35c9
Nico Ketzer <60319091+nicoketzer@users.noreply.github.com>
2020-06-25 21:41:43 +0200

    Create var.php
    Add first var.php

---
+++ var.php
@@ -0,0 +1,7 @@
+<?php
+    $db_bn = "[DATENBANK_NAME]";
+    $db_pw = "[DATENBANK_PASSWORT]";
+    $db_tbl = "[DATENBANK_TABELLE]";
+    $db_conn = "[DATENBANK_IP]";
+    $name = "[DIR_FOR_RUN_FILE]";
+?>
